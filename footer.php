21c9d316254f2766ecd51fee835d4c3db93f6714
Adam Grover <your_email@example.com>
2022-09-22 15:34:44 +0100

    Move footer content

---
+++ footer.php
@@ -0,0 +1,57 @@
+
+    <!-- FOOTER SECTION-->
+    <footer class="container-fluid">
+        <div class="footer-content container">
+            <div class="row footer-logo">
+                <div class="col-sm-4">
+                    <img src="wp-content/themes/kow22/Assets/logos/kings-icon.svg" alt="Kings Academy Icon" class="footer-icon">
+                </div>
+
+            </div>
+            <div class="row footer-text">
+                <div class="col-sm-4 footer-contact footer-column">
+                    <p>The Kings of Wessex Academy<br>Station Road, Cheddar<br>Somerset BS27 3AQ</p>
+                    <p>Headteacher: Dave Wiltshire</p>
+                    <p><i class="fa-solid fa-phone"></i><span class="footer-spacer"></span>01934742608</p>
+                    <p><a href=""><i class="fa-solid fa-envelope"></i></i><span class="footer-spacer"></span>Email Us</a></p>
+                    <p class="mb-5"><a href=""><i class="fa-solid fa-location-dot"></i></i><span class="footer-spacer"></span>Get Directions</a></p>
+
+                    <div class="footer-social"><a href="https://twitter.com/kowessex" target="_blank" ><i class="fa-brands fa-twitter fa-2xl"></i></a><span class="footer-spacer"></span><span class="footer-spacer"></span><a href="https://www.facebook.com/kowessex/" target="_blank"><i class="fa-brands fa-facebook-f fa-2xl"></i></a></div>
+
+                    <p class="mt-4">A charitable company limited by guarantee<br>
+                        Registered in England<br>
+                        Company Number 7348580</p>
+                    
+                </div>
+                <div class="col-sm-4 footer-links footer-column">
+                    <h4 class="footer heading mb-3">Quick Links</h4>
+                    <a href="" class="footer-link">Download Prospectus</a>
+                    <a href="" class="footer-link">Firefly</a>
+                    
+                </div>
+                
+
+                <div class="col-sm-4 footer-accessability d-flex-column footer-column">
+                    <div class="accesability-top">
+                        <h4 class="footer heading mb-3">Accessability</h4>
+                        <a href="" class="footer-link">Site Map</a>
+                        <a href="" class="footer-link">Terms of Use</a>
+                        <a href="" class="footer-link">Privacy Policy</a> 
+                        <a href="" class="footer-link">Cookie Usage</a> 
+                    </div>
+
+                    <a href="https://wessexlearningtrust.co.uk" target="_blank"><img class="wlt-white mt-5" src="wp-content/themes/kow22/Assets/logos/wlt-white.png" alt=""></a>               
+                </div>
+
+        </div>
+    </footer>
+    
+
+  
+    <?php
+
+wp_footer();
+?>
+
+</body>
+</html>
