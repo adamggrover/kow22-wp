5712bb238ad1bb45ed612af771376ca2ab1afca4
Adam Grover <your_email@example.com>
2022-09-23 14:17:06 +0100

    Paste dummy sidebar nav content

--- page.php
+++ page.php
@@ -43,8 +43,35 @@
 
                     </div>
                 </div>
-                <div class="col-lg-3 p-4" id="sidebar-section-nav-col">
+                <div class="col-lg-3" id="sidebar-section-nav-col">
                     <div id="sidebar-section-nav">
+                        <div id="SideNav">
+                            <div class="list-group panel">
+                              <a href="#" class="list-group-item list-group-item-success" data-parent="#SideNav">Home</a>
+                              <a href="#demo3" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#SideNav">Our Academy<i class="fa fa-caret-down nav-arrow"></i></a>
+                              <div class="collapse" id="demo3">
+                                <a href="#SubMenu1" class="list-group-item" data-toggle="collapse" data-parent="#SubMenu1">Subitem 1 <i class="fa fa-caret-down nav-arrow"></i></a>
+                                <div class="collapse list-group-submenu" id="SubMenu1">
+                                  <a href="#" class="list-group-item" data-parent="#SubMenu1">Subitem 1 a</a>
+                                  <a href="#" class="list-group-item" data-parent="#SubMenu1">Subitem 2 b</a>
+                                  <a href="#SubSubMenu1" class="list-group-item" data-toggle="collapse" data-parent="#SubSubMenu1">Subitem 3 c <i class="fa fa-caret-down nav-arrow"></i></a>
+                                  <div class="collapse list-group-submenu list-group-submenu-1" id="SubSubMenu1">
+                                    <a href="#" class="list-group-item" data-parent="#SubSubMenu1">Sub sub item 1</a>
+                                    <a href="#" class="list-group-item" data-parent="#SubSubMenu1">Sub sub item 2</a>
+                                  </div>
+                                  <a href="#" class="list-group-item" data-parent="#SubMenu1">Subitem 4 d</a>
+                                </div>
+                                <a href="javascript:;" class="list-group-item">Subitem 2</a>
+                                <a href="javascript:;" class="list-group-item">Subitem 3</a>
+                              </div>
+                              <a href="#demo4" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#SideNav">Item 4  <i class="fa fa-caret-down nav-arrow"></i></a>
+                              <div class="collapse" id="demo4">
+                                <a href="" class="list-group-item">Subitem 1</a>
+                                <a href="" class="list-group-item">Subitem 2</a>
+                                <a href="" class="list-group-item">Subitem 3</a>
+                              </div>
+                            </div>
+                          </div>
 
                     </div>
 
@@ -52,12 +79,6 @@
             </div>
         </div>
        
-
-        
-    
-
-
-
    </main>
 
 
