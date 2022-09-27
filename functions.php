6920c2d29c70db157e647a381d4af368da40d838
Adam Grover <your_email@example.com>
2022-09-27 10:12:19 +0100

    Update script links. move jquery to footer

--- functions.php
+++ functions.php
@@ -75,10 +75,11 @@ function kow22_register_scripts(){
 wp_enqueue_script('kow22-fontawesome', 'https://kit.fontawesome.com/c3ed52941f.js', array());
 wp_enqueue_script('kow22-aos', 'https://unpkg.com/aos@2.3.1/dist/aos.js', array());
 wp_enqueue_script('kow22-popper', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js', array(), '5.2.0', true);
-wp_enqueue_script('kow22-jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js', array(), '3.6.1'); 
+wp_enqueue_script('kow22-jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js', array(), '3.6.1', true); 
 wp_enqueue_script('kow22-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js', array(), '3.4', true); 
-wp_enqueue_script('kow22-slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/slick.min.js', array('kow22-jquery'), '3.4', true);
+wp_enqueue_script('kow22-slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array(), '1.8.1', true);
 wp_enqueue_script('kow22-main', get_template_directory_uri().'/Assets/js/main.js', array('kow22-jquery', 'kow22-slick', 'kow22-popper'), '1.1', true);
+wp_enqueue_script('kow22-main', get_template_directory_uri().'/Assets/js/slick.js', array('kow22-main'), '1.0', true);
 
 }
 
