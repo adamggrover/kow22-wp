395dcb8c4cd8f9f2d52dbab73bb3b4c160985db9
Adam Grover <your_email@example.com>
2022-09-28 14:36:44 +0100

    Add breadcrumbs and social share links

--- page.php
+++ page.php
@@ -1,21 +1,25 @@
 <?php
     get_header();
+
+
+
+
 ?>
 
 <div class="container" id="main-page-area">
             <div class="content-header row">
                 <div class="content-header-breadcrumb col-lg-5 col-md-12">
                     <i class="fa-solid fa-house fa-xs"></i><span class="breadcrumb-spacer"></span>
-                    <h6 class="mb-0 content-header-breadcrumb-links">Home / Our Academy / Welcome</h6>
+                    <h6 class="mb-0 content-header-breadcrumb-links"><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></h6>
                     
 
                 </div>
                 <div class="content-header-social col-lg-4 col-md-12">
                     <h6 class="mb-0 text-muted">Share This Page</h6><span class="breadcrumb-spacer"></span>
                     <div class="content-area-social-links text-muted">
-                        <i class="fa-brands fa-twitter fa-sm ps-2"></i>
-                        <i class="fa-brands fa-facebook-f fa-sm ps-2"></i>
-                        <i class="fa-solid fa-envelope fa-sm ps-2"></i>
+                        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><i class="fa-brands fa-twitter fa-sm ps-2"></i></a>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f fa-sm ps-2"></i></a>
+                        <a href="mailto:office@kowessex.co.uk"><i class="fa-solid fa-envelope fa-sm ps-2"></i></a>   
 
                     </div>
                     
@@ -46,29 +50,9 @@
                 <div class="col-lg-3" id="sidebar-section-nav-col">
                     <div id="sidebar-section-nav">
                         <div id="SideNav">
-                            <div class="list-group panel">
-                              <a href="#" class="list-group-item list-group-item-success" data-parent="#SideNav">Home</a>
-                              <a href="#demo3" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#SideNav">Our Academy<i class="fa fa-caret-down nav-arrow"></i></a>
-                              <div class="collapse" id="demo3">
-                                <a href="#SubMenu1" class="list-group-item" data-toggle="collapse" data-parent="#SubMenu1">Subitem 1 <i class="fa fa-caret-down nav-arrow"></i></a>
-                                <div class="collapse list-group-submenu" id="SubMenu1">
-                                  <a href="#" class="list-group-item" data-parent="#SubMenu1">Subitem 1 a</a>
-                                  <a href="#" class="list-group-item" data-parent="#SubMenu1">Subitem 2 b</a>
-                                  <a href="#SubSubMenu1" class="list-group-item" data-toggle="collapse" data-parent="#SubSubMenu1">Subitem 3 c <i class="fa fa-caret-down nav-arrow"></i></a>
-                                  <div class="collapse list-group-submenu list-group-submenu-1" id="SubSubMenu1">
-                                    <a href="#" class="list-group-item" data-parent="#SubSubMenu1">Sub sub item 1</a>
-                                    <a href="#" class="list-group-item" data-parent="#SubSubMenu1">Sub sub item 2</a>
-                                  </div>
-                                  <a href="#" class="list-group-item" data-parent="#SubMenu1">Subitem 4 d</a>
-                                </div>
-                                <a href="javascript:;" class="list-group-item">Subitem 2</a>
-                                <a href="javascript:;" class="list-group-item">Subitem 3</a>
-                              </div>
-                              <a href="#demo4" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#SideNav">Item 4  <i class="fa fa-caret-down nav-arrow"></i></a>
-                              <div class="collapse" id="demo4">
-                                <a href="" class="list-group-item">Subitem 1</a>
-                                <a href="" class="list-group-item">Subitem 2</a>
-                                <a href="" class="list-group-item">Subitem 3</a>
+                        	
+                    
+    
                               </div>
                             </div>
                           </div>
--- single.php
+++ single.php
@@ -6,16 +6,16 @@
             <div class="content-header row">
                 <div class="content-header-breadcrumb col-lg-5 col-md-12">
                     <i class="fa-solid fa-house fa-xs"></i><span class="breadcrumb-spacer"></span>
-                    <h6 class="mb-0 content-header-breadcrumb-links">Home / Our Academy / Welcome</h6>
+                    <h6 class="mb-0 content-header-breadcrumb-links"><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></h6>
                     
 
                 </div>
                 <div class="content-header-social col-lg-4 col-md-12">
                     <h6 class="mb-0 text-muted">Share This Page</h6><span class="breadcrumb-spacer"></span>
                     <div class="content-area-social-links text-muted">
-                        <i class="fa-brands fa-twitter fa-sm ps-2"></i>
-                        <i class="fa-brands fa-facebook-f fa-sm ps-2"></i>
-                        <i class="fa-solid fa-envelope fa-sm ps-2"></i>
+                        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><i class="fa-brands fa-twitter fa-sm ps-2"></i></a>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f fa-sm ps-2"></i></a>
+                        <a href="mailto:office@kowessex.co.uk"><i class="fa-solid fa-envelope fa-sm ps-2"></i></a>   
 
                     </div>
                     
