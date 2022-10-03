f34b9007ae37a4408a2c2e62dff6ad2e6283a7e1
Adam Grover <your_email@example.com>
2022-10-03 13:18:25 +0100

    Remove text muted class from content header

--- page.php
+++ page.php
@@ -15,8 +15,8 @@
 
                 </div>
                 <div class="content-header-social col-lg-4 col-md-12">
-                    <h6 class="mb-0 text-muted">Share This Page</h6><span class="breadcrumb-spacer"></span>
-                    <div class="content-area-social-links text-muted">
+                    <h6 class="mb-0">Share This Page</h6><span class="breadcrumb-spacer"></span>
+                    <div class="content-area-social-links">
                         <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><i class="fa-brands fa-twitter fa-sm ps-2"></i></a>
                         <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f fa-sm ps-2"></i></a>
                         <a href="mailto:office@kowessex.co.uk"><i class="fa-solid fa-envelope fa-sm ps-2"></i></a>   
