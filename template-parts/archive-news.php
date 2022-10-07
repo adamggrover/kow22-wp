87c3f199b8c8aeedc1237a1c39c905412c6fd3b1
Adam Grover <your_email@example.com>
2022-10-07 15:58:19 +0100

    Add news content to cards

---
+++ template-parts/archive-news.php
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+  <div class="col">
+  <a href="https://www.google.com" class="news-card" id="news-card-container">
+    <div class="card h-100">
+      <img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="...">
+      <div class="card-body" id ="news-card-body1">
+        <h6 class="card-link-text news-tags">TECHNOLOGY, COMPETITIONS</h6>
+        <h5 class="card-link-text news-title"><?php the_title();?></h5>
+        
+        <p class="card-link-text news-date"><small><?php the_date(); ?></small></p>
+      </div>
+    </div>
+    <div class="overlay" id="overlay1" 
+                        onMouseOver="opacityFunction(); zIndexFuction();"
+                        
+                        onMouseOut="opacityFunctionR(); zIndexFuctionR();"
+                        style="  position: absolute;
+                        opacity: 0;
+                        top: 0;
+                        bottom: 0;
+                        left: 0;
+                        right: 0;
+                        height: 100%;
+                        width: 100%;
+                        
+                        transition: .5s ease;
+                        background-color: rgba(192, 214, 48, 0.90);">
+                        
+                
+                    </div>
+    </a>
+  </div>
+
+
+
+
+<div class="col-sm-4 p-4">
+                    
+                    
+            
+                    <div class="img-container img-link-1" style="position: relative;
+                    width: 100%;" >
+        
+        
+                        <a href="https://www.google.com" class="news-card">
+                        
+                    
+                            <div class="card shadow">
+                                    
+                                <img src="wp-content/themes/kow22/Assets/images/3d-printer.jpg" class="card-img-top" style="width: 100%;
+                                max-height: 70%;
+                                object-fit:cover;" alt="...">
+        
+                                
+                            
+                            <div class="card-body p-3 d-flex-column" id ="news-card-body1">
+                                <h6 class="card-link-text news-tags">TECHNOLOGY, COMPETITIONS</h6>
+                                <h4 class="card-link-text news-title">Technology Competition</h4>
+                                <p class="card-link-text news-date"><small>Sept 2022</small></p>
+                            </div>
+                        </div>
+        
+                        
+                        <div class="overlay" id="overlay1" 
+                        onMouseOver="opacityFunction(); zIndexFuction();"
+                        
+                        onMouseOut="opacityFunctionR(); zIndexFuctionR();"
+                        style="  position: absolute;
+                        opacity: 0;
+                        top: 0;
+                        bottom: 0;
+                        left: 0;
+                        right: 0;
+                        height: 100%;
+                        width: 100%;
+                        
+                        transition: .5s ease;
+                        background-color: rgba(192, 214, 48, 0.90);">
+                        
+                
+                    </div>
+                    
+        
+                    </a>
+                    </div>
+                    </div>
+        
