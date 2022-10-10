2369ad461226ff4b8ee0796d98440cd8309670f2
Adam Grover <your_email@example.com>
2022-10-10 12:13:36 +0100

    Disable overlay hover effect
    Couldn't get hover effect to work for different posts so disabled
    it for the time being by commenting out section

--- template-parts/archive-news.php
+++ template-parts/archive-news.php
@@ -15,7 +15,7 @@
         <p class="card-link-text news-date"><small><?php the_date(); ?></small></p>
       </div>
     </div>
-    <div class="overlay" id="overlay1" 
+   <!--<div class="overlay" id="overlay1" 
                         onMouseOver="opacityFunction(); zIndexFuction();"
                         
                         onMouseOut="opacityFunctionR(); zIndexFuctionR();"
@@ -32,61 +32,9 @@
                         background-color: rgba(192, 214, 48, 0.90);">
                         
                 
-                    </div>
+                    </div>-->
     </a>
   </div>
 
 
 
-
-<div class="col-sm-4 p-4">
-                    
-                    
-            
-                    <div class="img-container img-link-1" style="position: relative;
-                    width: 100%;" >
-        
-        
-                        <a href="https://www.google.com" class="news-card">
-                        
-                    
-                            <div class="card shadow">
-                                    
-                                <img src="wp-content/themes/kow22/Assets/images/3d-printer.jpg" class="card-img-top" style="width: 100%;
-                                max-height: 70%;
-                                object-fit:cover;" alt="...">
-        
-                                
-                            
-                            <div class="card-body p-3 d-flex-column" id ="news-card-body1">
-                                <h6 class="card-link-text news-tags">TECHNOLOGY, COMPETITIONS</h6>
-                                <h4 class="card-link-text news-title">Technology Competition</h4>
-                                <p class="card-link-text news-date"><small>Sept 2022</small></p>
-                            </div>
-                        </div>
-        
-                        
-                        <div class="overlay" id="overlay1" 
-                        onMouseOver="opacityFunction(); zIndexFuction();"
-                        
-                        onMouseOut="opacityFunctionR(); zIndexFuctionR();"
-                        style="  position: absolute;
-                        opacity: 0;
-                        top: 0;
-                        bottom: 0;
-                        left: 0;
-                        right: 0;
-                        height: 100%;
-                        width: 100%;
-                        
-                        transition: .5s ease;
-                        background-color: rgba(192, 214, 48, 0.90);">
-                        
-                
-                    </div>
-                    
-        
-                    </a>
-                    </div>
-                    </div>
-        
