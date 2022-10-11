88f20657131951315fbc110ab18ecc605b17e46b
Adam Grover <your_email@example.com>
2022-10-11 14:53:50 +0100

    Remove tags/ Add excerpt

--- archive-news.php
+++ archive-news.php
@@ -9,8 +9,9 @@
     <div class="card h-100">
       <a href="<?php the_permalink();?>"><img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="..."></a>
       <div class="card-body" id ="news-card-body1">
-        <h6 class=""><?php the_tags('',' | ');?></h6>
+        <!--<h6 class=""><?php the_tags('',' | ');?></h6>-->
         <a href="<?php the_permalink();?>"><h5 class=""><?php the_title();?></h5></a>
+        <p><?php the_excerpt();?></p>
         
         <p class="card-link-text news-date"><small><?php the_date(); ?></small></p>
       </div>
