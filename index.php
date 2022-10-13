b83ec5694f11b54df0ce593173aa3fc03fcf20d4
Adam Grover <your_email@example.com>
2022-10-13 10:16:48 +0100

    Give content header <small> tag

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <div class="content-header row">
                     <div class="content-header-breadcrumb col-lg-6 col-md-12">
                         <i class="fa-solid fa-house fa-xl"></i><span class="breadcrumb-spacer"></span>
-                        <h6 class="mb-0 content-header-breadcrumb-links"><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></h6>
+                        <h6 class="mb-0 content-header-breadcrumb-links"><small><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></small></h6>
                         
 
                     </div>
                     <div class="content-header-social col-lg-6 col-md-12">
-                        <h6 class="mb-0">Share This Page</h6><span class="breadcrumb-spacer"></span>
+                        <h6 class="mb-0"><small>Share This Page</small></h6><span class="breadcrumb-spacer"></span>
                         <div class="content-area-social-links">
                             <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><i class="fa-brands fa-twitter fa-sm ps-2 fa-xl"></i></a>
                             <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f ps-2 fa-xl"></i></a>
