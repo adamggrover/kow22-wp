dd1668e8cfb3c9e76da447fe04a68b16843f8962
Adam Grover <your_email@example.com>
2022-10-14 16:05:05 +0100

    add wp latest post hover effect

--- archive-matchreports-layout.php
+++ archive-matchreports-layout.php
@@ -16,24 +16,7 @@
       <p class="card-link-text news-date"><small><?php the_date(); ?></small></p>
     </div>
   </div>
- <!--<div class="overlay" id="overlay1" 
-                      onMouseOver="opacityFunction(); zIndexFuction();"
-                      
-                      onMouseOut="opacityFunctionR(); zIndexFuctionR();"
-                      style="  position: absolute;
-                      opacity: 0;
-                      top: 0;
-                      bottom: 0;
-                      left: 0;
-                      right: 0;
-                      height: 100%;
-                      width: 100%;
-                      
-                      transition: .5s ease;
-                      background-color: rgba(192, 214, 48, 0.90);">
-                      
-              
-                  </div>-->
+
   
 </div>
 
