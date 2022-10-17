790358fa767511a3be0ecd37525226f6fed37395
Adam Grover <your_email@example.com>
2022-10-17 14:40:53 +0100

    Make news cards pull from news archive

--- front-page.php
+++ front-page.php
@@ -323,18 +323,37 @@
                      
 
 
-<?php
-if (have_posts()):
-while (have_posts()) : the_post();
+                  <?php
+                    $newsPosts = new WP_Query( array( 'pagename' => 'news', 'posts_per_page' => '3' ) );
+                    
 
-    get_template_part('archive', 'news');
+                    while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
 
+                    
+                    <div class="col">
     
-endwhile;
-else:
-echo '<p>Sorry, no posts matched your criteria.</p>';
-endif;
-?>
+                    <div id="#news-card-container">
+                     
+                    <div class="card h-100 news-card">
+                      <a href="<?php the_permalink();?>"><img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="..."></a>
+                      <div class="card-body" id ="news-card-body">
+                        <!--<h6 class=""><?php the_tags('',' | ');?></h6>-->
+                        <a href="<?php the_permalink();?>"><h5 class="news-card-title"><?php the_title();?></h5></a>
+                        <p><?php the_excerpt();?></p>
+                        
+                        <p class="card-link-text news-card-date"><small><?php the_date(); ?></small></p>
+                      </div>
+                      <div class="news-overlay"></div>
+                    </div>
+                    
+                   </div>
+                  
+                    
+                  </div>
+                    <?php endwhile; ?>
+
+                    
+
 
 
 
