fbff389cd24420c5dacb00a7af4fba3dc2e32d50
Adam Grover <your_email@example.com>
2022-10-21 13:42:43 +0100

    put news card content in <a> tag

--- archive-news.php
+++ archive-news.php
@@ -8,16 +8,27 @@
     
     <div id="#news-card-container">
      
-    <div class="card h-100 news-card">
-      <a href="<?php the_permalink();?>"><img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="..."></a>
-      <div class="card-body" id ="news-card-body">
-        <!--<h6 class=""><?php the_tags('',' | ');?></h6>-->
-        <a href="<?php the_permalink();?>"><h5 class="news-card-title"><?php the_title();?></h5></a>
-        <p><?php the_excerpt();?></p>
-        
-        <p class="card-link-text news-card-date"><small><?php the_date(); ?></small></p>
-      </div>
+        <div class="card h-100 news-card">
+          <a href="<?php the_permalink();?>">
+          
+            <div class="news-card-content">
+              <img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="...">
+            </a>
+              <div class="card-body" id ="news-card-body">
+                <!--<h6 class=""><?php the_tags('',' | ');?></h6>-->
+                <a href="<?php the_permalink();?>"><h5 class="news-card-title"><?php the_title();?></h5>
+                <p><?php the_excerpt();?></p>
+                
+                <p class="card-link-text news-card-date"><small><?php the_date(); ?></small></p>
+              </div>
+              
+            </div>
+            
+      
+      
+        </a>
       <div class="news-overlay"></div>
+      
     </div>
     
    </div>
